0f1ecbfda256f6eca563593653e565fba9771705
Adonis <adonisbecerra1219@gmail.com>
2023-08-03 16:41:11 -0600

    Se agregó el formulario de productos

---
+++ src/js/validacionesAgregarProducto.js
@@ -0,0 +1,117 @@
+const titulo = document.getElementById("titulo");
+const categoria = document.getElementById("categoria");
+const clave = document.getElementById("clave");
+const numStock = document.getElementById("numStock");
+const marca = document.getElementById("marca");
+const descripcion = document.getElementById("descripcion");
+//opcion multiple
+const precio = document.getElementById("precio");
+const descuento = document.getElementById("descuento");
+const checkInfo = document.getElementById("checkInfo");
+const boton = document.getElementById("registrar");
+
+
+boton.addEventListener("click", (function(event){
+
+    event.preventDefault();
+
+    let errores = validarDatos();
+    console.log(errores.length);
+
+    if (errores.length == 0){
+        let producto = {"name":titulo.value,"img":"./src/img/imgProductos/comidaPerro1.png", "description":descripcion.value, "precio":precio.value};
+        window.localStorage.setItem("producto", JSON.stringify(producto));
+        Swal.fire(
+            'Éxito',
+            'El producto fue agregado correctamente',
+            'success'
+          )
+
+    } else{
+        let strErrores;
+        errores.forEach((error)=>{
+            strErrores += error + "\n";
+        });
+    
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: strErrores,
+          })
+    }
+
+
+
+}));
+
+
+
+
+function validarDatos(){
+    var errores = [];
+    titulo.style.border="";
+    //
+    //QUITAR EL ESTILO A TODOS LOS BORDES
+    //
+
+
+if(!(titulo.value.length > 2 )){
+    titulo.style.border="solid 2px red"
+    errores.push("El titulo no es válido");
+}
+
+
+if(!(categoria.value.length > 2 )){
+    categoria.style.border="solid 2px red"
+    errores.push("La categoria no es válida");
+}
+
+if(!(clave.value.length > 2 )){
+    clave.style.border="solid 2px red"
+    errores.push("Las palabras clave no son válidas");
+}
+
+if(!(numStock.value >= 0 )){
+    numStock.style.border="solid 2px red"
+    errores.push("El número de Stock no es válido");
+}
+
+if(!(marca.value.length > 2 )){
+    marca.style.border="solid 2px red"
+    errores.push("La marca no es válido");
+}
+
+if(!(descripcion.value.length > 10 )){
+    descripcion.style.border="solid 2px red"
+    errores.push("La descripción no es válido");
+}
+
+if(!(precio.value > 10 )){
+    precio.style.border="solid 2px red"
+    errores.push("El precio no es válido");
+}
+
+if(!(descuento.value.length > 2 )){
+    descuento.style.border="solid 2px red"
+    errores.push("El descuento no es válido");
+}
+
+if(!(checkInfo.checked)){
+    checkInfo.style.border="solid 2px red"
+    errores.push("No se marcó checkbox de revisión de información");
+}
+
+return errores;
+
+
+}
+
+
+
+
+
+
+
+
+
+
